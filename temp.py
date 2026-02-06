e0adfe5e8ea4c0f3f2b183a6e53c70c85c856d07
Abdur Rehman Siddiqui <172410072+abdurrehmansiddiqui10498@users.noreply.github.com>
2026-02-06 09:11:58 +0500

    Add temperature conversion from Fahrenheit to Celsius

---
+++ temp.py
@@ -0,0 +1,4 @@
+for index in range(1,8):
+    Farhenheit = int(input("Please enter the temperature of the week (in Farhenheit)"))
+    celsius = (Farhenheit-32)*5/9
+    print("Your temperature in Celsius is",celsius)
